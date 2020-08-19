4c769298b617251bd147dcd5d0ba2b1edd81c0b4
Toshi <waykiyoharu@gmail.com>
2020-08-19 14:56:49 +0900

    propsを実装
    IndexFormに２種類のプロップスを当てて実装

--- src/components/IndexForm.vue
+++ src/components/IndexForm.vue
@@ -2,13 +2,13 @@
   <v-container>
     <v-row>
         <v-col class='feature'>
-        <h3>本サイトについて</h3>
-        <span>FEATURE</span>
-        <div>外出自粛が広く実施される昨今<br>
-        「Life Flower」は、改めて家庭で花を楽しむ体験を提案します<br>
-        皆さんの生活に彩りを添える花を１輪飾ってみませんか。<br>
-        あなたの街の素敵なお花屋さんと花々を楽しむ体験を<br>
-        私たちは提案します。</div>
+        <h3>{{title}}</h3>
+        <span>{{subTitle}}</span>
+        <div>{{text1}}<br>
+        {{text2}}<br>
+       {{text3}}<br>
+        {{text4}}<br>
+       {{text5}}</div>
         </v-col>
     </v-row>
 
@@ -25,6 +25,16 @@
 
 <script>
 export default {
+   name: "IndexForm",
+  props: {
+    title: String,
+    subTitle: String,
+    text1: String,
+    text2: String,
+    text3: String,
+    text4: String,
+    text5: String,
+  },
   
 }
 </script>
--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -3,9 +3,28 @@
     <v-row justify="center" align="center">
       <v-col cols="12">
       <TopForm/>
-      <IndexForm/>
+      <IndexForm
+       title= "本サイトについて"
+    subTitle= "FEATURE"
+    text1= "外出自粛が広く実施される昨今"
+    text2= "「Life Flower」は、改めて家庭で花を楽しむ体験を提案します"
+    text3= " 皆さんの生活に彩りを添える花を１輪飾ってみませんか。"
+    text4= "あなたの街の素敵なお花屋さんと花々を楽しむ体験を"
+    text5= "私たちは提案します。"
+      
+      
+      
+      />
       <TopSlideForm/>
-      <IndexForm/>
+      <IndexForm
+        :title= "test[0].title"
+    :subTitle= "test[0].subTitle"
+    :text1= "test[0].text1"
+    :text2= "test[0].text2"
+    :text3= "test[0].text3"
+    :text4= "test[0].text4"
+    :text5= "test[0].text5"
+      />
       </v-col>
     </v-row>
   </v-container>
@@ -25,6 +44,25 @@ export default{
         TopSlideForm,//ここの記載をtemplateで呼び出す際に使う
         
     },
+  data: () => {
+    return {      
+      test: [
+        { title: '使い方',
+         subTitle: 'あなたのお気に入りのお店をみつけよう',
+          text1: '現在地や任意の場所を指定して',
+          text2: '検索をスタート', 
+          text3: 'あなたの街の素敵なお店が見つかります。', 
+          text4: '気に入ったお店をフォローして', 
+          text5: '毎日投稿をチェックしましょう', },
+          //参考サイト：https://mebee.info/2020/02/24/post-5009/
+      ],
+    };
+  },
+   
+         
+ 
+      
+    
 }
 
 
